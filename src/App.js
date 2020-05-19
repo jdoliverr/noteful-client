491a3765392d777c1fb26cefbc74ad0ff6d62679
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-19 08:19:47 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+
+      </div>
+    );
+  }
+}
+
+export default App;
