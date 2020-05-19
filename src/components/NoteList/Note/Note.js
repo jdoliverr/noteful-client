33dfe1d4c3421d0e04a7383974c1dd2d92cd5dc2
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-19 12:40:35 -0700

    Added comopnents for Note and Folder

---
+++ src/components/NoteList/Note/Note.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NoteList extends Component {
+
+
+    render() {
+        return (
+            <div className="note-container">
+                <a href="/note" class="note-link">{this.props.name}</a>
+                <div className="note-bottom">
+                    <div className="note-mod">Date Modified: {this.props.date}</div>
+                    <button className="note-delete">Delete Note</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NoteList;
--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
+import Note from './Note/Note';
 
 class NoteList extends Component {
-
-
     render() {
-
         const notes = this.props.notes.map(note => {
             const date = new Date(note.modified);
-            return (
-                <div className="note-container" key={note.id}>
-                    <a href="/note" class="note-link">{note.name}</a>
-                    <div className="note-bottom">
-                        <div className="note-mod">Date Modified: {date.toLocaleString()}</div>
-                        <button className="note-delete">Delete Note</button>
-                    </div>
-                </div>
-            )
+            return <Note name={note.name} key={note.id} date={date.toLocaleString()} />
         })
 
         return (
