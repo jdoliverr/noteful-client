601bbf43a8fcea566bda8050d9372ad399ca4a19
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-19 23:30:50 -0700

    Cleaned up routes code

--- src/App.js
+++ src/App.js
@@ -1,102 +1,18 @@
 import React, { Component } from 'react'
-import { Route, NavLink, Switch } from 'react-router-dom'
-import Sidebar from './components/Sidebar/Sidebar'
-import NoteList from './components/NoteList/NoteList'
-import SingleFolder from './components/Sidebar/SingleFolder/SingleFolder'
-import NotePage from './components/NotePage/NotePage'
-import MainPage from './components/MainPage/MainPage'
+import { NavLink } from 'react-router-dom'
 import Routes from './Routes'
-import STORE from './store'
+
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      folders: STORE.folders,
-      notes: STORE.notes,
-      folderId: 'all'
-      
-    }
-  }
-
-  changeNotes = (folderId) => {
-    console.log(`changeNotes ran`)
-    const currentNotes = STORE.notes.filter(notes => notes.id !== folderId);
-    this.setState({
-      notes: currentNotes
-    })
-  }
-
-  resetFolderId = () => {
-    this.setState({
-      folderId: 'all'
-    })
-  }
-  
-  changeFolder = (folderId) => {
-    console.log(`changeFolder ran and state changed`)
-    this.setState({
-      folderId: folderId
-    })
-  }
-
-  // One route shows both main sidebar and note list
-  // Make a grouping/wrapping component to use with route
-  // pass match in and do filter on App within the render
-
-
   render() {
-    console.log(this.state.folderId);
-    console.log(this.state.notes);
-    
-
     return (
       <div className='App'>
           <header>
             <NavLink to='/' className="header-link">Noteful</NavLink>
           </header>
           <main>
-            <Switch>
-                <Route 
-                  exact path='/' 
-                  render={() => {
-                    const folderId = 'all'
-                    return (
-                      <MainPage 
-                        folders={this.state.folders}
-                        notes={this.state.notes}
-                        folderId={folderId}
-                      />)
-                  }}
-                />
-                <Route 
-                  path='/folder/:folderId'
-                  render={(routeProps) => {
-                    const currentNotes = this.state.notes.filter(note => note.folderId === routeProps.match.params.folderId);
-                    return (
-                      <MainPage
-                        folders={this.state.folders}
-                        notes={currentNotes}
-                        folderId = {routeProps.match.params.folderId}
-                      />
-                    )
-                  }}
-                />
-                <Route 
-                  path='/note/:noteId'
-                  render={(routeProps) => {
-                    const currentNote = this.state.notes.find(note => note.id === routeProps.match.params.noteId);
-                    console.log(currentNote)
-                    return (
-                      <NotePage
-                        folders={this.state.folders}
-                        currentNote={currentNote}
-                        onHomeClick={() => routeProps.history.push('/')}
-                      />)
-                  }}
-                />
-            </Switch>
+            <Routes />
           </main>
       </div>
     );
@@ -104,4 +20,3 @@ class App extends Component {
 }
 
 export default App;
-
--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,64 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import NotePage from './components/NotePage/NotePage'
 import MainPage from './components/MainPage/MainPage'
+import STORE from './store'
 
-export default function Routes() {
-    return (
-        <Switch>
-            <Route exact path='/' component={MainPage} />
-        </Switch>
-    )
+
+class Routes extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          folders: STORE.folders,
+          notes: STORE.notes,
+          folderId: 'all'
+          
+        }
+    }
+    
+    render() {
+        return (
+            <Switch>
+                <Route 
+                    exact path='/' 
+                    render={() => {
+                        const folderId = 'all'
+                        return (
+                        <MainPage 
+                            folders={this.state.folders}
+                            notes={this.state.notes}
+                            folderId={folderId}
+                        />)
+                    }}
+                />
+                <Route 
+                    path='/folder/:folderId'
+                    render={(routeProps) => {
+                        const currentNotes = this.state.notes.filter(note => note.folderId === routeProps.match.params.folderId);
+                        return (
+                        <MainPage
+                            folders={this.state.folders}
+                            notes={currentNotes}
+                            folderId = {routeProps.match.params.folderId}
+                        />
+                        )
+                    }}
+                />
+                <Route 
+                    path='/note/:noteId'
+                    render={(routeProps) => {
+                        const currentNote = this.state.notes.find(note => note.id === routeProps.match.params.noteId);
+                        console.log(currentNote)
+                        return (
+                        <NotePage
+                            folders={this.state.folders}
+                            currentNote={currentNote}
+                            onHomeClick={() => routeProps.history.push('/')}
+                        />)
+                    }}
+                />
+            </Switch>
+        )
+    }
 }
+
+export default Routes
